bc52861d08b04ac149109bdfeb8c5db442da6f85
Wender Machado <devwendermachado@gmail.com>
2025-07-10 16:11:51 -0300

    adiciona state para mudar estaado dos games junto com função de adicionar games

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import { useState } from "react"
 
 export default function App() {
+  const [games, setGames] = useState([])
   const [title, setTitle] = useState("")
   const [cover, setCover] = useState("")
-  const handleSubmit = (ev)=>{
-    ev.preventDefalt()
 
+  const addGame= (({title, cover})=>{
+    const id = Math.floor(Math.random()* 10000)
+    const game = {id, title, cover}
+    setGames(state => [...state, game])
+  })
+  const handleSubmit = (ev)=>{
+    ev.preventDefault()
+    addGame({title, cover})
+    setTitle("")
+    setCover("")
   }
   return (
       <div className="app">
