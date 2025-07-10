0ba4bc70fd9cb68b1e98b0e37aaf5a4cfb752035
Wender Machado <devwendermachado@gmail.com>
2025-07-10 15:29:07 -0300

    adiciona state iniciais de manipulação: -State de manipulação de titulo -State de manipulaçã de capa -funcção de controle de submit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
+import { useState } from "react"
 
 export default function App() {
+  const [title, setTitle] = useState("")
+  const [cover, setCover] = useState("")
+  const handleSubmit = (ev)=>{
+    ev.preventDefalt()
+
+  }
   return (
-    <>
-      <div id="app">
-        
+      <div className="app">
+      <h1>Biblioteca de Jogos</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="title">Título:</label>
+          <input 
+          type="text" 
+          id="title" 
+          value={title} 
+          onChange={(ev) => setTitle(ev.target.value)} />
+        </div>
+        <div>
+          <label htmlFor="cover">Capa:</label>
+          <input 
+          type="text" 
+          id="cover" 
+          value={cover} 
+          onChange={(ev) => setCover(ev.target.value)} />
+        </div>
+        <button>Adicionar</button>
+      </form>
+      <div className="games">
+
       </div>
-    </>
+    </div>
   )
 }
 
